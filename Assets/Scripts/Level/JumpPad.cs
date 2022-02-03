0c7300f4f7b4b73dd14f82b165089803d634a234
Stefan Alfredsson <stefan@argentrealms.com>
2022-02-03 11:28:12 +0100

    Created a simple Jump Pad.

--- Assets/Scripts/Level/JumpPad.cs
+++ Assets/Scripts/Level/JumpPad.cs
@@ -2,5 +2,26 @@ using UnityEngine;
 
 public class JumpPad : MonoBehaviour
 {
-    //TODO Implement JumpPad functionality
+    [SerializeField] private float jumpForce = 1500f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Classic null check.
+        // var otherRigidbody = other.GetComponent<Rigidbody>();
+        // if (otherRigidbody != null)
+        //     otherRigidbody.AddForce(transform.up * jumpForce);
+
+        //Null check with early return.
+        // var otherRigidbody = other.GetComponent<Rigidbody>();
+        // if (otherRigidbody == null)
+        //     return;
+        // otherRigidbody.AddForce(transform.up * jumpForce);
+
+        //Null check using null propagation operator. Only runs AddForce() if Rigidbody is NOT null.
+        other.GetComponent<Rigidbody>()?.AddForce(transform.up * jumpForce);
+
+        //Only evaluates to true if the Rigidbody component is found, in which case TryGetComponent() returns true.
+        // if (other.TryGetComponent(out Rigidbody rb))
+        //     rb.AddForce(transform.up * jumpForce);
+    }
 }
